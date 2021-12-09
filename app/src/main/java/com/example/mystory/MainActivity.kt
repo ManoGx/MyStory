13b70ac2f683eefc765cf35a9c72189b634bdf55
ManoGx <ali00016@hotmail.com>
2021-12-09 22:31:43 +0300

    navigation drawer completed

--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private var textViewEmail:TextView?=null
+    private var drawerLayout:DrawerLayout? = null
+    private var toolbarView:Toolbar?=null
+    private var navigationView:NavigationView ? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +27,56 @@ class MainActivity : AppCompatActivity() {
         connectViews()
 
         textViewEmail?.text = email //لازم استدعيها بعد دالة الكونكت فيوز عشان ماتكون قيمة الايميل نل
+        setSupportActionBar(toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //هذه الدالة الي بتطلع لنا علامة الرجوع الي بحوله للهمبرقر
+
+        setupDrawer()
+        updateEmailInHeader(email!!)
+        drawerClicks()
+    }
+    private fun updateEmailInHeader(email:String){//هذه الدالة خاصة لعرض الهيدر واستدعاء الايميل المدخل
+        val headerView = navigationView?.getHeaderView(0)
+        val textViewEmail = headerView?.findViewById<TextView>(R.id.tvEmail) //عشان نستدعي الايميل عشان نحدثه بالسطر الي تحت دا
+        textViewEmail?.text = email
+    }
+    private fun setupDrawer(){
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                drawerLayout?.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> true
+        }
     }
     private fun connectViews(){
         textViewEmail = findViewById(R.id.tvEmail)
+        drawerLayout = findViewById(R.id.drawer)
+        toolbarView = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navView)
     }
+
+    private fun drawerClicks(){
+        navigationView?.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->{
+                    drawerLayout?.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.logout ->{
+                    finish() //لو ماحطيتها... لما تضغط زر رجوع بيرجعك للشاشة الي قبل
+                    val i = Intent(this,LoginActivity::class.java)
+                    startActivity(i)
+                    true
+                }
+                else -> true
+            }
+        }
+    }
+
 }
